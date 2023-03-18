83a8d5858c17c589f7a4553da798fe880b0655c6
basithbdev <125488164+basithbdev@users.noreply.github.com>
2023-03-18 19:36:20 +0530

    feat: adds sign up controller

---
+++ BlitzTest/Controllers/SignUpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlitzTest.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
