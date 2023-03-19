5f640b38438905f96f1da68e30f362ad8281d8f8
basithbdev <125488164+basithbdev@users.noreply.github.com>
2023-03-19 09:48:58 +0530

    feat: creates admin controller

---
+++ BlitzTest/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlitzTest.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult ManageUsers()
+        {
+            return View();
+        }
+
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        public IActionResult AddSubCategory()
+        {
+            return View();
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        public IActionResult ManageOrders()
+        {
+            return View();
+        }
+
+
+    }
+}
