965c3077122037c8bb0d0fba3cfcb379c3eb9706
basithbdev <125488164+basithbdev@users.noreply.github.com>
2023-03-20 17:43:08 +0530

    refactor: adds cart method

---
+++ BlitzTest/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlitzTest.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult AllProducts()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
